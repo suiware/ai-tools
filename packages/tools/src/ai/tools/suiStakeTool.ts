b24d83dfc43a3745e9a68dd04d61dba2620e000f
Konstantin Komelin <konstantin.komelin@gmail.com>
2025-04-23 17:44:57 +0200

    tools: Limited the amount staked by the suiStakeTool to not less than 1 SUI.

--- packages/tools/src/ai/tools/suiStakeTool.ts
+++ packages/tools/src/ai/tools/suiStakeTool.ts
@@ -5,7 +5,7 @@ import { SuiStakingService } from '../../services/SuiStakingService'
 export const suiStakeTool = tool({
   description: 'Stake Sui',
   parameters: z.object({
-    amount: z.number().describe('The amount of SUI to stake'),
+    amount: z.number().min(1).describe('The amount of SUI to stake'),
   }),
   execute: async ({ amount }) => {
     const suiStakingService = new SuiStakingService()
