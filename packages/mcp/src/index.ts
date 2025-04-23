72d4ca71a491704bfbe06d9deff2762f591583b1
Konstantin Komelin <konstantin.komelin@gmail.com>
2025-04-23 18:41:51 +0200

    mcp: Refactored MCP server in order to export some helper functions from the package.

---
+++ packages/mcp/src/index.ts
@@ -0,0 +1,27 @@
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js'
+import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js'
+import { getSuiwareAiTools } from '@suiware/ai-tools'
+import { mapVercelToolToMcpTool } from './utils/mappers'
+import { getPackageMeta } from './utils/misc'
+
+export const createMcpServer = (): McpServer => {
+  const packageMeta = getPackageMeta()
+
+  const server = new McpServer({
+    name: packageMeta?.description || 'Sui AI Tools by Suiware',
+    version: packageMeta?.version || '0.0.0',
+  })
+
+  for (const [name, tool] of Object.entries(getSuiwareAiTools())) {
+    // Hope we will have a native Tool type soon https://github.com/modelcontextprotocol/typescript-sdk/issues/369
+    const mcpTool = mapVercelToolToMcpTool(tool)
+    server.tool(name, mcpTool.description, mcpTool.paramsSchema, mcpTool.cb)
+  }
+
+  return server
+}
+
+export const startMcpServer = async (server: McpServer) => {
+  const transport = new StdioServerTransport()
+  await server.connect(transport)
+}
--- packages/mcp/src/server.ts
+++ packages/mcp/src/server.ts
@@ -1,25 +1,9 @@
-import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js'
-import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js'
-import { getSuiwareAiTools } from '@suiware/ai-tools'
-import { mapVercelToolToMcpTool } from './utils/mappers'
-import { getPackageMeta } from './utils/misc'
+import { createMcpServer, startMcpServer } from './'
 
-const packageMeta = getPackageMeta()
-
-const server = new McpServer({
-  name: packageMeta?.description || 'Sui AI Tools by Suiware',
-  version: packageMeta?.version || '0.0.0',
-})
-
-for (const [name, tool] of Object.entries(getSuiwareAiTools())) {
-  // Hope we will have a native Tool type soon https://github.com/modelcontextprotocol/typescript-sdk/issues/369
-  const mcpTool = mapVercelToolToMcpTool(tool)
-  server.tool(name, mcpTool.description, mcpTool.paramsSchema, mcpTool.cb)
-}
+const server = createMcpServer()
 
 async function main() {
-  const transport = new StdioServerTransport()
-  await server.connect(transport)
+  await startMcpServer(server)
 }
 
 main()
